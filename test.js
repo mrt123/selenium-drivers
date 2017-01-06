484f27396b5cb0e24aecc23be6d0bd5ed6aa0987
Tomasz <tomasz.szawara@gmail.com>
2017-01-06 21:17:02 +0000

    modularisation WIP1

--- test.js
+++ test.js
@@ -2,18 +2,17 @@ var seleniumDrivers = require('./lib/selenium-drivers');
 
 seleniumDrivers.run('chrome').then(function () {
     
-
     var webdriver = require('selenium-webdriver'),
-        By = webdriver.By,
         until = webdriver.until;
 
     var driver = new webdriver.Builder()
-        .forBrowser('firefox')
+        .forBrowser('chrome')
         .build();
 
-    //driver.get('http://www.google.com/ncr');
-    //driver.findElement(By.name('q')).sendKeys('webdriver');
-    //driver.findElement(By.name('btnG')).click();
-    //driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-    //driver.quit();
+    driver.get('http://www.google.com/ncr');
+    driver.wait(until.titleIs('Google'), 1000);
+    driver.quit().then(function () {
+        console.log('Test passed');
+    });
+    
 });
